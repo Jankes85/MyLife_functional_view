95bec113ca1b6da1f250dc6c18ad9f9116b44867
Jankes85 <wojtek_jankowski@o2.pl>
2023-02-27 13:13:04 +0100

    working

--- mylifie/mylife/settings.py
+++ mylifie/mylife/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from mylifie.my_data import s_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_g5h-6f065j=qf%b$+e(k-xru@arsa-fbwa40upa2wmd#wn8^='
+SECRET_KEY = s_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
